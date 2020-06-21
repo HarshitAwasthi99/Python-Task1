efa81681cb82dbe47b947a4f73807eebe03d324f
harshitawasthi@gmail.com <harshitawasthi2607@gmail.com>
2020-06-22 00:15:53 +0530

    Python task1

---
+++ int_float.py
@@ -0,0 +1,39 @@
+# integer values
+a=10
+b=20
+print("sum=",a+b)
+print("sub=",a-b)
+print("multiply=",a*b)
+print("Division=",a/b)
+print("mod=",a%b)
+
+
+# concatination 
+num1='100'
+num2='200'
+print(num1+num2)
+
+# TO add 2 no.
+num1=int(num1)
+num2=int(num2)
+print(num1+num2)
+
+#Comparision
+a=2
+b=7
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+
+
+#Float values
+
+r=19.6
+g=10.6
+print("Sum=",r+g)
+print("sub=",r-g)
+
+print(round(3.75))
+
+print(round(3.75,1))
---
+++ percentage.py
@@ -0,0 +1,29 @@
+#Full name (Concatination must be used)
+First_name="Harshit"
+Last_name="Awasthi"
+name=First_name+ ' ' +Last_name
+print(name)
+
+collage_name="DIT UNIVERSITY"
+address="Mussoorie Road ,Dehradun,UTTARAKHAND"
+message=collage_name+' ' +address
+print(message)
+
+
+#Following are name of students whom percentage is to calculated
+
+rohit=87
+virat=91
+sachin=100
+rahul=54
+hardik=50
+
+#Average of student
+
+total=(rohit+virat+sachin+rahul+hardik)
+percentage=(total/500)*100
+
+print(total)
+
+print(percentage)
+
